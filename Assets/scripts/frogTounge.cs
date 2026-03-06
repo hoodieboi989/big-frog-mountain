e2fe2941ec05c47bcfd240204d6b699d85897069
hoodieboi989 <hansemat025@alpinesd.org>
2026-03-06 13:00:01 -0700

    Update frogTounge.cs

--- Assets/scripts/frogTounge.cs
+++ Assets/scripts/frogTounge.cs
@@ -1,5 +1,6 @@
 using UnityEngine;  
-using UnityEngine.InputSystem; // Import the new Input System  
+using UnityEngine.InputSystem;
+using System.Collections;
   
 public class FrogTongue : MonoBehaviour  
 {  
@@ -17,28 +18,47 @@ public class FrogTongue : MonoBehaviour
     void OnDisable() { controls.Gameplay.Disable(); }  
   
     void Start() { lineRenderer.positionCount = 0; }  
-  
-    void ShootTongue()  
+
+    IEnumerator AnimateTongue(Vector2 direction)  
     {  
-        Vector2 mouseScreen = controls.Gameplay.MousePosition.ReadValue<Vector2>();  
-        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);  
-        mouseWorld.z = transform.position.z;  
+        float speed = 60f; // How fast the tongue extends  
+        lineRenderer.positionCount = 2;    
+        lineRenderer.SetPosition(0, transform.position);    
+  
+    Vector2 start = transform.position;    
+    Vector2 end = start;    
+  
+    Vector2 target = start + direction * tongueDistance;  
+    float traveled = 0f;  
     
-        Vector2 direction = ((Vector2)mouseWorld - (Vector2)transform.position).normalized;  
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, tongueDistance);  
+    while (traveled < tongueDistance)  
+    {  
+        float step = speed * Time.deltaTime;  
+        traveled += step;  
+        Vector2 nextPoint = Vector2.Lerp(start, target, traveled / tongueDistance);  
     
-        lineRenderer.positionCount = 2;  
-        lineRenderer.SetPosition(0, transform.position);  
+        lineRenderer.SetPosition(1, nextPoint);  
     
+        RaycastHit2D hit = Physics2D.Raycast(start, direction, traveled);  
         if (hit.collider != null)  
         {  
             lineRenderer.SetPosition(1, hit.point);  
+            yield break;  
         }  
-        else  
-        {  
-            // Draw to max tongue distance in the aimed direction  
-            Vector2 endPoint = (Vector2)transform.position + direction * tongueDistance;  
-            lineRenderer.SetPosition(1, endPoint);  
-        }  
+        yield return null;  
+}  
+    // If we reach here, tongue fully extended without hitting    
+    // (We'll add retraction here next!)    
+    }  
+
+    void ShootTongue()  
+    {  
+        StopAllCoroutines(); // Stop any current tongue animation  
+        
+        Vector2 mouseScreen = controls.Gameplay.MousePosition.ReadValue<Vector2>();  
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);  
+        mouseWorld.z = transform.position.z;  
+        Vector2 direction = ((Vector2)mouseWorld - (Vector2)transform.position).normalized;  
+        StartCoroutine(AnimateTongue(direction));  
     }  
 }  
